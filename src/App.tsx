26f10996f7b39e63a4b98e520559783cbcbafece
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-05-09 19:03:59 -0400

    Effet fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
+import { Canvas } from "@react-three/fiber";
+import Ring from "./Ring";
+import { OrbitControls } from "@react-three/drei";
+
+export default function App() {
   return (
-    <>
-      <h1 className="text-3xl">Hello world</h1>
-    </>
+    <main className="h-screen w-screen">
+      <Canvas className="bg-black">
+        <ambientLight intensity={1} />
+        <Ring
+          radius={2}
+          height={4}
+          segments={32}
+          // text="F R A I S C O M M E U N G A R D O N "
+          text="G M D N I L O C G M D N I L O C"
+        />
+        <OrbitControls />
+      </Canvas>
+    </main>
   );
 }
-
-export default App;
---
+++ src/Ring.tsx
@@ -0,0 +1,61 @@
+import { MeshTransmissionMaterial, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+interface Props {
+  text: string;
+  radius: number;
+  height: number;
+  segments: number;
+}
+
+export default function Ring({ text, radius, height, segments }: Props) {
+  const ref = useRef<any>();
+
+  // Rotation du texte
+  useFrame(() => {
+    ref.current.rotation.y += 0.01;
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.z += 0.01;
+  });
+
+  // Calcul des positions pour le texte
+  const textPositions: { x: number; z: number }[] = [];
+  const angleStep = (2 * Math.PI) / text.length;
+  for (let i = 0; i < text.length; i++) {
+    const angle = i * angleStep;
+    const x = radius * Math.cos(angle);
+    const z = radius * Math.sin(angle);
+    textPositions.push({ x, z });
+  }
+
+  return (
+    <group ref={ref}>
+      {/* Cylindre */}
+      <mesh>
+        <cylinderGeometry args={[radius, radius, height, segments]} />
+        <MeshTransmissionMaterial
+          backside
+          backsideThickness={5}
+          thickness={2}
+        />
+      </mesh>
+
+      {/* Texte */}
+      {text.split("").map((char: string, index: number) => (
+        <Text
+          key={index}
+          position={[textPositions[index].x, 0, textPositions[index].z]}
+          rotation={[0, -angleStep * index + Math.PI / 2, 0]}
+          fontSize={0.3}
+          lineHeight={1}
+          letterSpacing={0.02}
+          color="white"
+          textAlign="center"
+        >
+          {char}
+        </Text>
+      ))}
+    </group>
+  );
+}
